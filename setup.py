047386435e0c85811b0292f2cdf39a00f028e715
DJ Passey <djpassey@djs-macbook-pro.lan>
2022-12-13 12:36:31 -0500

    adding deps to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='qsPEMs',
       author='Alice Schwarze',
       author_email='alice.c.schwarze@dartmouth.edu',
       license='MIT',
-      packages=['numpy'],
+      packages=['qspems'],
+      requires=['numpy'],
       zip_safe=False)
