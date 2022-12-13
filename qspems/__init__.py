b79943373d0366cb0786b4fde0d2954f2e80ed1a
DJ Passey <djpassey@djs-macbook-pro.lan>
2022-12-13 12:07:11 -0500

    wrap code in importable python packaging

---
+++ qspems/__init__.py
@@ -0,0 +1,1 @@
+from .qsPEMs import *
---
+++ qspems/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qsPEMs',
+      version='0.1',
+      description='Network inference via process motifs',
+      url='https://github.com/acuschwarze/network-inference-via-process-motifs',
+      author='Alice Schwarze',
+      author_email='alice.c.schwarze@dartmouth.edu',
+      license='MIT',
+      packages=['numpy'],
+      zip_safe=False)
